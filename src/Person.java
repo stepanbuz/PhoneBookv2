74cbda0752dc65339234dd81355688e4e2f059c3
Stepan <my@email>
2023-06-17 12:25:05 +0500

    New class

--- src/Person.java
+++ src/Person.java
@@ -14,4 +14,56 @@ class Person {
         this.gender = gender;
         this.type = type;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString(){return this.name + "=" + this.surname + "=" + this.phone+ "=" + this.age+ "=" + this.type + "=" + this.gender;}
+    public String inFo(){return this.name + "=" + this.surname + "=" + this.phone+ "=" + this.age+ "=" + this.type + "=" + this.gender;}
 }
