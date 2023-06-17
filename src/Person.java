c7737e1fa47642341cdddc94a476a63a753a6c45
Stepan <my@email>
2023-06-17 11:37:05 +0500

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,17 @@
+class Person {
+    private String name;
+    private String surname;
+    private String phone;
+    private String age;
+    private String gender;
+    private String type;
+
+    public Person(String name, String surname, String phone, String age, String gender, String type) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.age = age;
+        this.gender = gender;
+        this.type = type;
+    }
+}
