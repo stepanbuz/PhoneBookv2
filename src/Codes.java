34e06964b0b79fb12fb6c485758e40f9fefb3ef3
Stepan <my@email>
2023-06-17 12:30:34 +0500

    New class

--- src/Codes.java
+++ src/Codes.java
@@ -1,2 +1,21 @@
-public class Codes {
+enum Codes {
+    CREATE(0), READ(1), UPDATE(2), DELETE(3);
+
+    public int getCode() {
+        return code;
+    }
+
+    Codes(int code) {
+        this.code = code;
+    }
+
+    public static Codes getEnumByCode(int code) throws Exception {
+        for (Codes c : Codes.values()) {
+            if (c.code == code) {
+                return c;
+            }
+        }
+        throw new Exception("No equals");
+    }
+    private final int code;
 }
